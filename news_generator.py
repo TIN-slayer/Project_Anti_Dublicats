8549e7fb308c694ae88e9fee35c7a0d10a081ad6
TIN-slayer <dimka_dmit@mail.ru>
2021-05-25 23:12:35 +0300

    alpha_2

--- news_generator.py
+++ news_generator.py
@@ -4,18 +4,28 @@ f = open('old_news.txt', 'w')
 alf = []
 alf.extend([chr(i) for i in range(ord('a'), ord('z'))])
 alf.extend([chr(i) for i in range(ord('A'), ord('Z'))])
-alf.extend([' ', ',', ':', '-'])
+alf.extend([' ', ' ', ' ', ' ', ' ', ' ', ' ', ',', ':', '-'])
 ans = []
-for _ in range(randint(10, 20)):
+for _ in range(randint(50, 100)):
     row = ''
     for _ in range(randint(5, 10)):
         buf = '\n'
         for _ in range(randint(25, 50)):
             buf += choice(alf)
         row += buf
-    row += '\n///'
-    for _ in range(randint(1, 3)):
-        ans.append(row)
+    row += '\n'
+    for qtty in range(randint(2, 3)):
+        new = list(row.replace('\n', '&'))
+        n = len(new) * randint(0, 5) // 100
+        s = 0
+        while s != n:
+            pos = randint(0, len(new) - 1)
+            key = choice(alf)
+            if new[pos] not in [key, '&', ' ']:
+                new[pos] = key
+                s += 1
+        post = ''.join(new).replace('&', '\n') + '///'
+        ans.append(post)
 shuffle(ans)
 for i in ans:
     f.write(i)
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import sqlite3
+from fuzzywuzzy import fuzz
 
 con = sqlite3.connect('data.db')
 cur = con.cursor()
@@ -6,15 +7,20 @@ cur.execute("DELETE FROM posts")
 con.commit()
 f = open('old_news.txt')
 file = f.read().split('///')[:-1]
-print(file)
 ans = open('new_news.txt', 'w')
 for i in file:
     flag = True
     for row in cur.execute('SELECT text FROM posts'):
-        if row[0] == i:
+        line = row[0]
+        # print(fuzz.token_sort_ratio(row[0], i))
+        if fuzz.token_sort_ratio(row[0], i) >= 80:
             flag = False
             break
     if flag:
         cur.execute(f"INSERT INTO posts (text) VALUES ('{i}')")
         ans.write(i + '///')
+ans.close()
 con.commit()
+op = open('new_news.txt')
+op_ans = op.read().split('///')[:-1]
+print(f'Удалено дубликатов: {len(file) - len(op_ans)}')
